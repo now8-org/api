18cc805838129fa8b272dc735cd7ae24dd02fc70
m0wer <m0wer@autistici.org>
2021-08-13 16:31:47 +0300

    refactor: initial structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ntapi",
+    version="0.0.0",
+    description="Estimated time of arrival for public transport vehicles.",
+    author="m0wer",
+    author_email="m0wer@autisitci.org",
+    license="GPLv3",
+    long_description=open("README.md").read(),
+    packages=find_packages(exclude=("tests",)),
+    entry_points={"console_scripts": []},
+    install_requires=[
+        "fastapi",
+        "gunicorn",
+        "pydantic",
+    ],
+)
