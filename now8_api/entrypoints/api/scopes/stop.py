c7494cfc29a675100749951ed96a23f121a6ac3f
m0wer <m0wer@autistici.org>
2021-11-08 07:47:09 +0300

    docs(swagger): large response warning

--- now8_api/entrypoints/api/scopes/stop.py
+++ now8_api/entrypoints/api/scopes/stop.py
@@ -17,6 +17,12 @@ service: Service = Service()
     summary="Get all stops in the city.",
 )
 async def stop_api() -> List[Dict[str, Union[str, float]]]:
+    """DO NOT CALL THIS ENDPOINT FROM THE SWAGGER UI.
+
+    It will return a list with thousands of stop information dictionaries
+    that can freeze Swagger UI. You can call the endpoint directly (without
+    `/docs` in the path) with a web browser or cURL for example.
+    """
 
     result = await service.all_stops()
 
