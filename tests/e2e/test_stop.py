22a7ca60a6b331351b237fcf9990ee3e10338576
m0wer <m0wer@autistici.org>
2021-12-13 20:12:40 +0300

    test(e2e): check exact output for stop

--- tests/e2e/test_stop.py
+++ tests/e2e/test_stop.py
@@ -15,6 +15,15 @@ class TestStop:
 
         assert response.status_code == 200
         assert isinstance(response.json(), dict)
+        assert response.json()["par_8_99987"] == {
+            "code": "99987",
+            "id": "par_8_99987",
+            "latitude": 40.242103577,
+            "longitude": -4.190074921,
+            "name": "MÉNTRIDA-ESTACIÓN DE SERVICIO",
+            "route_ways": [{"id": "358", "way": 0}, {"id": "767", "way": 0}],
+            "zone": "E1",
+        }
 
 
 class TestStopInfo:
@@ -23,6 +32,24 @@ class TestStopInfo:
 
         assert response.status_code == 200
         assert isinstance(response.json(), dict)
+        assert response.json() == {
+            "code": "17491",
+            "id": "par_8_17491",
+            "latitude": 40.296051025,
+            "longitude": -3.457335711,
+            "name": "RONDA SUR-HOSPITAL DEL SURESTE",
+            "route_ways": [
+                {"id": "269", "way": 0},
+                {"id": "279", "way": 1},
+                {"id": "280", "way": 1},
+                {"id": "281", "way": 1},
+                {"id": "678", "way": 0},
+                {"id": "688", "way": 1},
+                {"id": "689", "way": 1},
+                {"id": "690", "way": 1},
+            ],
+            "zone": "B3",
+        }
 
 
 class TestStopEstimation:
