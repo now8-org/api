6735f92667e886901acfd5c0b901439ef38ea344
m0wer <m0wer@autistici.org>
2021-12-13 20:05:49 +0300

    feat(entrypoints): add response models and cache to route endpoints

--- tests/e2e/test_route.py
+++ tests/e2e/test_route.py
@@ -3,25 +3,37 @@ from fastapi.testclient import TestClient
 from now8_api.entrypoints.api.main import api
 
 
-class TestRoute:
-    @pytest.fixture(autouse=True)
-    def initialize_test_client(self):
-        self.client = TestClient(api)
+@pytest.fixture
+def client():
+    with TestClient(api) as client:
+        yield client
 
-    def test_route(self):
-        response = self.client.get("/route")
+
+class TestRoute:
+    def test_route(self, client):
+        response = client.get("/route")
 
         assert response.status_code == 200
         assert isinstance(response.json(), dict)
+        assert response.json()["999"] == {
+            "code": "6",
+            "color": "E60003",
+            "id": "999",
+            "name": "UNIVERSIDAD REY JUAN CARLOS-URB. P.GUADARRAMA",
+            "transport_type": 3,
+        }
 
 
 class TestRouteInfo:
-    @pytest.fixture(autouse=True)
-    def initialize_test_client(self):
-        self.client = TestClient(api)
-
-    def test_madrid_route(self):
-        response = self.client.get("/route/633/info")
+    def test_madrid_route(self, client):
+        response = client.get("/route/633/info")
 
         assert response.status_code == 200
         assert isinstance(response.json(), dict)
+        assert response.json() == {
+            "code": "151",
+            "color": "8EBF42",
+            "id": "633",
+            "name": "MADRID (Plaza de Castilla) - ALCOBENDAS",
+            "transport_type": 3,
+        }
